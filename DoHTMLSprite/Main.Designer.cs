bdc15ae5508dcb88dae24154dad74f510c98cdde
Alexandru Tertisco <alex@diffstudios.com>
2026-06-22 15:51:20 +0300

    Init commit

---
+++ DoHTMLSprite/Main.Designer.cs
@@ -0,0 +1,146 @@
+﻿namespace DoHTMLSprite
+{
+    partial class Main
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Main));
+            this.btnSourceFolder = new System.Windows.Forms.Button();
+            this.lblSourceFolder = new System.Windows.Forms.Label();
+            this.btnCreate = new System.Windows.Forms.Button();
+            this.lblDestinationFolder = new System.Windows.Forms.Label();
+            this.btnTargetFolder = new System.Windows.Forms.Button();
+            this.folderBrowser = new System.Windows.Forms.FolderBrowserDialog();
+            this.statusStrip = new System.Windows.Forms.StatusStrip();
+            this.lblStatus = new System.Windows.Forms.ToolStripStatusLabel();
+            this.statusStrip.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // btnSourceFolder
+            // 
+            this.btnSourceFolder.Location = new System.Drawing.Point(12, 12);
+            this.btnSourceFolder.Name = "btnSourceFolder";
+            this.btnSourceFolder.Size = new System.Drawing.Size(163, 23);
+            this.btnSourceFolder.TabIndex = 0;
+            this.btnSourceFolder.Text = "Source folder";
+            this.btnSourceFolder.UseVisualStyleBackColor = true;
+            this.btnSourceFolder.Click += new System.EventHandler(this.btnSourceFolder_Click);
+            // 
+            // lblSourceFolder
+            // 
+            this.lblSourceFolder.AutoSize = true;
+            this.lblSourceFolder.Location = new System.Drawing.Point(190, 17);
+            this.lblSourceFolder.Name = "lblSourceFolder";
+            this.lblSourceFolder.Size = new System.Drawing.Size(10, 13);
+            this.lblSourceFolder.TabIndex = 1;
+            this.lblSourceFolder.Text = "-";
+            // 
+            // btnCreate
+            // 
+            this.btnCreate.Enabled = false;
+            this.btnCreate.Location = new System.Drawing.Point(12, 70);
+            this.btnCreate.Name = "btnCreate";
+            this.btnCreate.Size = new System.Drawing.Size(163, 23);
+            this.btnCreate.TabIndex = 2;
+            this.btnCreate.Text = "Create sprite";
+            this.btnCreate.UseVisualStyleBackColor = true;
+            this.btnCreate.Click += new System.EventHandler(this.btnCreate_Click);
+            // 
+            // lblDestinationFolder
+            // 
+            this.lblDestinationFolder.AutoSize = true;
+            this.lblDestinationFolder.Location = new System.Drawing.Point(190, 46);
+            this.lblDestinationFolder.Name = "lblDestinationFolder";
+            this.lblDestinationFolder.Size = new System.Drawing.Size(10, 13);
+            this.lblDestinationFolder.TabIndex = 4;
+            this.lblDestinationFolder.Text = "-";
+            // 
+            // btnTargetFolder
+            // 
+            this.btnTargetFolder.Enabled = false;
+            this.btnTargetFolder.Location = new System.Drawing.Point(12, 41);
+            this.btnTargetFolder.Name = "btnTargetFolder";
+            this.btnTargetFolder.Size = new System.Drawing.Size(163, 23);
+            this.btnTargetFolder.TabIndex = 3;
+            this.btnTargetFolder.Text = "Destination folder";
+            this.btnTargetFolder.UseVisualStyleBackColor = true;
+            this.btnTargetFolder.Click += new System.EventHandler(this.btnTargetFolder_Click);
+            // 
+            // statusStrip
+            // 
+            this.statusStrip.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.lblStatus});
+            this.statusStrip.Location = new System.Drawing.Point(0, 98);
+            this.statusStrip.Name = "statusStrip";
+            this.statusStrip.Size = new System.Drawing.Size(589, 22);
+            this.statusStrip.SizingGrip = false;
+            this.statusStrip.Stretch = false;
+            this.statusStrip.TabIndex = 5;
+            // 
+            // lblStatus
+            // 
+            this.lblStatus.Name = "lblStatus";
+            this.lblStatus.Size = new System.Drawing.Size(11, 17);
+            this.lblStatus.Text = "-";
+            // 
+            // Main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(589, 120);
+            this.Controls.Add(this.statusStrip);
+            this.Controls.Add(this.lblDestinationFolder);
+            this.Controls.Add(this.btnTargetFolder);
+            this.Controls.Add(this.btnCreate);
+            this.Controls.Add(this.lblSourceFolder);
+            this.Controls.Add(this.btnSourceFolder);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Main";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "CSS Sprite Creator";
+            this.statusStrip.ResumeLayout(false);
+            this.statusStrip.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnSourceFolder;
+        private System.Windows.Forms.Label lblSourceFolder;
+        private System.Windows.Forms.Button btnCreate;
+        private System.Windows.Forms.Label lblDestinationFolder;
+        private System.Windows.Forms.Button btnTargetFolder;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowser;
+        private System.Windows.Forms.StatusStrip statusStrip;
+        private System.Windows.Forms.ToolStripStatusLabel lblStatus;
+    }
+}
+
---
+++ DoHTMLSprite/Main.cs
@@ -0,0 +1,147 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Threading;
+using System.IO;
+
+namespace DoHTMLSprite
+{
+    public partial class Main : Form
+    {
+        string sourceFolder = string.Empty, destinationFolder = string.Empty;
+        List<SpriteImage> imagesToProcess = new List<SpriteImage>();
+        int MaxWidth = 0, MaxHeight = 0;
+
+        public Main()
+        {
+            InitializeComponent();
+
+            if (!String.IsNullOrEmpty(DoHTMLSprite.Properties.Settings.Default.lastdir))
+                folderBrowser.SelectedPath = DoHTMLSprite.Properties.Settings.Default.lastdir;
+        }
+
+        private void btnSourceFolder_Click(object sender, EventArgs e)
+        {
+            if (folderBrowser.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                DoHTMLSprite.Properties.Settings.Default.lastdir = sourceFolder = folderBrowser.SelectedPath;
+                DoHTMLSprite.Properties.Settings.Default.Save();
+                btnTargetFolder.Enabled = btnCreate.Enabled = true;
+                lblSourceFolder.Text = sourceFolder;
+                imagesToProcess = GetFiles();
+            }
+        }
+
+        private void btnTargetFolder_Click(object sender, EventArgs e)
+        {
+            if (folderBrowser.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                destinationFolder = folderBrowser.SelectedPath;
+                lblDestinationFolder.Text = destinationFolder;
+            }
+        }
+
+        private void btnCreate_Click(object sender, EventArgs e)
+        {
+            if (String.IsNullOrEmpty(destinationFolder))
+                destinationFolder = lblDestinationFolder.Text = sourceFolder;
+            
+            DoSpriteImg();
+        }
+
+
+        /// <summary>
+        /// Returns a list with the image files available to be added to the sprite
+        /// </summary>
+        /// <returns></returns>
+        private List<SpriteImage> GetFiles()
+        {
+            DirectoryInfo dir = new DirectoryInfo(sourceFolder);
+            
+            if (!dir.Exists)
+                lblStatus.Text = "Source directory not found!";
+            
+            List<FileInfo> files = dir.GetFiles("*.*", SearchOption.TopDirectoryOnly).ToList();
+            List<SpriteImage> spriteImages = new List<SpriteImage>();
+
+            files = (from f in files
+                     where
+                        f.Name.EndsWith(".jpg", StringComparison.InvariantCultureIgnoreCase) ||
+                        f.Name.EndsWith(".jpeg", StringComparison.InvariantCultureIgnoreCase) ||
+                        f.Name.EndsWith(".png", StringComparison.InvariantCultureIgnoreCase) ||
+                        f.Name.EndsWith(".bmp", StringComparison.InvariantCultureIgnoreCase) ||
+                        f.Name.EndsWith(".gif", StringComparison.InvariantCultureIgnoreCase)
+                     orderby f.Name
+                     select f).ToList();
+
+            lblStatus.Text = (files.Count == 0) ? "No image file was found!" : files.Count.ToString()+" images found.";
+
+            foreach (FileInfo f in files)
+            {
+                SpriteImage simage = new SpriteImage();
+                simage.imgInfo = f;
+                simage.sImage = Image.FromFile(f.FullName);
+                spriteImages.Add(simage);
+
+                if (simage.sImage.Size.Width > MaxWidth)
+                    MaxWidth = simage.sImage.Size.Width;
+
+                MaxHeight += simage.sImage.Size.Height+5;
+            }
+
+            return spriteImages;
+        }
+
+        private void DoSpriteImg()
+        {
+            int lastY = 0;
+            StringBuilder sbCSS = new StringBuilder();
+            StringBuilder sbHTML = new StringBuilder();
+
+            Bitmap sprite = new Bitmap(MaxWidth, MaxHeight);
+            Graphics g = Graphics.FromImage(sprite);
+            //g.Clear(Color.Red);
+
+            for (int i = 0; i < imagesToProcess.Count(); i++)
+            { 
+                g.DrawImage(imagesToProcess[i].sImage, 0, lastY, imagesToProcess[i].sImage.Size.Width, imagesToProcess[i].sImage.Size.Height);
+                sbCSS.AppendLine(string.Format(".csssprite{0} {{ background-position: 0 {1}px; width: {2}px; height: {3}px; margin:5px 0 }}", i, (lastY > 0) ? (-lastY) : 0, imagesToProcess[i].sImage.Size.Width, imagesToProcess[i].sImage.Size.Height));
+                lastY += imagesToProcess[i].sImage.Size.Height+5;
+
+                // create sample html page
+                sbHTML.AppendLine("<div class=\"spriteImage csssprite" + i.ToString() + "\"></div>");
+            }
+
+            
+            // write image file
+            string fileName = Guid.NewGuid().ToString();
+            sprite.Save(destinationFolder + @"\" + fileName + ".png", System.Drawing.Imaging.ImageFormat.Png);
+            sbCSS.Insert(0, ".spriteImage { background-image: url(" + fileName + ".png) } ");
+            FileStream fsCSS = new FileStream(destinationFolder + @"\" + fileName +".css", FileMode.Create);
+            StreamWriter swCSS = new StreamWriter(fsCSS);
+            swCSS.Write(sbCSS.ToString());
+            swCSS.Flush();
+            fsCSS.Close();
+            // write HTML file
+            sbHTML.Insert(0, "<head><LINK REL=StyleSheet HREF=\"" + fileName + ".css\" TYPE=\"text/css\"></head><body>");
+            sbHTML.Append("</body>");
+            FileStream fsHTML = new FileStream(destinationFolder + @"\" + fileName + ".html", FileMode.Create);
+            StreamWriter swHTML = new StreamWriter(fsHTML);
+            swHTML.Write(sbHTML.ToString());
+            swHTML.Flush();
+            fsHTML.Close();
+            MessageBox.Show("Sprite image, css file and demo html done.", "Done", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+    }
+}
+
+public struct SpriteImage
+{
+    public Image sImage;
+    public FileInfo imgInfo;
+}
